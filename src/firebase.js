390f42f84a9b46db5888c2188c8bcace40ad805a
Morne van Zyl <mornevanzyl@Mornes-Mac-mini.local>
2021-04-22 15:56:55 +0100

    Basic authentication module set up

---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+import firebase from "firebase/app";
+import 'firebase/auth';
+import 'firebase/storage';
+import 'firebase/firestore';
+
+const app = firebase.initializeApp({
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID
+})
+
+// const auth = app.auth();
+// const storage = app.storage();
+// const database = app.firestore();
+// const timestamp = firebase.firestore.FieldValue.serverTimestamp;
+
+// export { auth, storage, database, timestamp };
+export default app; 
